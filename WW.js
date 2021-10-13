2935ad97affca9c38c1bdd5adde1f500c9fe786a
AntonErlandsson4 <70326194+AntonErlandsson4@users.noreply.github.com>
2021-10-13 14:26:34 +0200

    Add files via upload

---
+++ WW.js
@@ -0,0 +1,13 @@
+
+var x = 0; 
+
+    function STINKY(){
+        if(x==0){
+            document.getElementById("STINKY_POOP").innerHTML="STINKY";
+            x++;
+        }
+        else if(x==1){
+            document.getElementById("STINKY_POOP").innerHTML="POOP";
+            x--;
+        }
+    }
